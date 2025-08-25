0c913a3c680fb24179f59954c624e3b5978815cd
Dylan Keck <dkeck@cnm.edu>
2025-08-25 17:16:04 -0600

    updated structure

--- backend/src/routes/index.ts
+++
@@ -1,5 +0,0 @@
-import { Router } from "express";
-const router = Router();
-router.use("/auth", (await import("./auth.routes.js")).default);
-router.use("/jobs", (await import("./jobs.routes.js")).default);
-export default router;
